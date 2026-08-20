46721982925508701d79cc04abec00a61009500c
MMP-MAIN <hwanman@icloud.com>
2026-08-21 02:33:00 +0900

    위코 GA4 속성으로 관리자 데이터 연결

--- admin/config.js
+++ admin/config.js
@@ -1,5 +1,5 @@
 window.WECO_ADMIN_CONFIG = {
-  propertyId: '535644624',
+  propertyId: '549904933',
   googleClientId: '377832352654-220ju5k8p8rpueh3s4b6qaabs53ioif8.apps.googleusercontent.com',
   metaAppId: 'YOUR_META_APP_ID',
   // 위코 전용 Meta 광고계정 ID 확인 후 입력합니다. 다른 브랜드 계정은 연결하지 않습니다.
