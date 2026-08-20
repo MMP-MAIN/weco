a6d7449c815d269309b4ff3d61d433ce7c5f93bf
MMP-MAIN <hwanman@icloud.com>
2026-08-21 01:20:42 +0900

    관리자에서 가식 Meta 광고계정 제거

--- admin/config.js
+++ admin/config.js
@@ -2,5 +2,6 @@ window.WECO_ADMIN_CONFIG = {
   propertyId: '535644624',
   googleClientId: '377832352654-220ju5k8p8rpueh3s4b6qaabs53ioif8.apps.googleusercontent.com',
   metaAppId: 'YOUR_META_APP_ID',
-  metaAdAccountId: '976230043878568'
+  // 위코 전용 Meta 광고계정 ID 확인 후 입력합니다. 다른 브랜드 계정은 연결하지 않습니다.
+  metaAdAccountId: 'YOUR_WECO_META_AD_ACCOUNT_ID'
 }
